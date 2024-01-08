e8266ec2d28441d8f713819bced3b6e041e53203
ashutosh <dev.ashutoshh@gmail.com>
2024-01-08 10:09:32 +0530

    updated

---
+++ lecture5.2/q2.c
@@ -0,0 +1,58 @@
+#include "stdio.h"
+
+int main()
+{
+    int num1, num2, num3, num4;
+
+    printf("Enter first number : ");
+    scanf("%d", &num1);
+    printf("Enter first number : ");
+    scanf("%d", &num2);
+    printf("Enter first number : ");
+    scanf("%d", &num3);
+    printf("Enter first number : ");
+    scanf("%d", &num4);
+
+    if (num1 > num2)
+    {
+        if (num1 > num3)
+        {
+            if (num1 > num4)
+            {
+                printf("%d is biggest", num1);
+            }
+            else
+                printf("%d is biggest", num4);
+        }
+        else
+        {
+            if (num3 > num4)
+            {
+                printf("%d is the biggest", num3);
+            }
+            else
+                printf("%d is the biggest", num4);
+        }
+    }
+    else
+    {
+        if (num2 > num3)
+        {
+            if (num2 > num4)
+            {
+                printf("%d is biggest", num2);
+            }
+            else
+                printf("%d is biggest", num4);
+        }
+        else
+        {
+            if (num3 > num4)
+            {
+                printf("%d is the biggest", num3);
+            }
+            else
+                printf("%d is the biggest", num4);
+        }
+    }
+}
---
+++ lecture5.2/readme.md
@@ -0,0 +1,5 @@
+<img src="Screenshot 2024-01-07 115027.png"></img>
+
+<p>output for question 1</p>
+<img src="Screenshot 2024-01-08 100822.png"></img>
+<p>output for question 1</p>
