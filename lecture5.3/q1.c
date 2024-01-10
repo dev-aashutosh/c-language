c50741676a382cbfc6abc731180ed6ed6834afae
ashutosh <dev.ashutoshh@gmail.com>
2024-01-10 19:33:34 +0530

    updated

--- lecture5.3/q1.c
+++ lecture5.3/q1.c
@@ -3,6 +3,7 @@
 int main()
 {
     int num1, num2, num3;
+
     printf("Enter three numbers : ");
     scanf("%d %d %d", &num1, &num2, &num3);
     printf("The minimum of the three numbers is: %d\n",
---
+++ lecture5.3/readme.md
@@ -0,0 +1,5 @@
+<img src="./assets/1.png"></img>
+
+<p>output for question 1</p>
+<img src="./assets/2.png"></img>
+<p>output for question 2</p>
