145559110a7dfdf6792b6222ab60e8d113c25cf9
ashutosh <dev.ashutoshh@gmail.com>
2024-01-10 19:29:31 +0530

    uploaded new folder

--- lecture5.3/q1.c
+++ lecture5.3/q1.c
@@ -1,19 +1,11 @@
-#include "stdio.h"
+#include <stdio.h>
 
 int main()
 {
-    int n1, n2, n3, n4;
-    printf("Enter four numbers");
-    scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
-
-    n1 > n2
-        ? n1 > n3
-              ? n1 > n4
-                    ? printf("%d is greatest", n1)
-                    : printf("%d is greatest", n4)
-
-          : n3 > n4
-              ? printf("%d is the grestest", n3)
-              : printf("%d is the grestest", n3)
-        :
+    int num1, num2, num3;
+    printf("Enter three numbers : ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    printf("The minimum of the three numbers is: %d\n",
+           (num1 > num2) ? ((num2 > num3) ? num3 : num2) : num1);
+    return 0;
 }
