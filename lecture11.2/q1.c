ccb004e79c37d942a214a1ecb2e1591cc3339976
ashutosh <dev.ashutoshh@gmail.com>
2024-02-02 23:14:35 +0530

    updated

---
+++ lecture11.2/q1.c
@@ -0,0 +1,22 @@
+// Write a Program to find the reverse of a 1D array using a Chain of Pointers.
+#include "stdio.h"
+int main()
+{
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("array[%d] : ", i);
+        scanf("%d", &array[i]);
+    }
+
+    int *pointer;
+    pointer = &array[0]; // connecting
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("\n%d", *(pointer + i));
+    }
+}
